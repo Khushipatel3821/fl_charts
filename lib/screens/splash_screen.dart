fe6099a200ce2f98652566945b8a54fb2a8b8ef6
sojitrkh <Android@3821>
2025-09-09 16:27:19 +0530

    Splash Screen UI Change

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -12,42 +12,38 @@ class SplashScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Splash Screen", style: TextStyle(fontSize: 20)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  splashController.navigateToCartesianChart();
-                },
-                child: Text("Cartesian Chart"),
-              ),
-              SizedBox(height: 10,),
-              ElevatedButton(
-                onPressed: () {
-                  splashController.navigateToPieChart();
-                },
-                child: Text("Pie Chart"),
-              ),
-              SizedBox(height: 10,),
-              ElevatedButton(
-                onPressed: () {
-                  splashController.navigateToRadialChart();
-                },
-                child: Text("Radial Chart"),
-              ),
-              SizedBox(height: 10,),
-              ElevatedButton(
-                onPressed: () {
-                  splashController.navigateToLiveChart();
-                },
-                child: Text("Live Chart"),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                splashController.navigateToCartesianChart();
+              },
+              child: Text("Cartesian Chart", style: TextStyle(fontSize: 20),),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: () {
+                splashController.navigateToPieChart();
+              },
+              child: Text("Pie Chart", style: TextStyle(fontSize: 20),),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: () {
+                splashController.navigateToRadialChart();
+              },
+              child: Text("Radial Chart", style: TextStyle(fontSize: 20),),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: () {
+                splashController.navigateToLiveChart();
+              },
+              child: Text("Live Chart", style: TextStyle(fontSize: 20),),
+            ),
+          ],
         ),
       ),
     );
